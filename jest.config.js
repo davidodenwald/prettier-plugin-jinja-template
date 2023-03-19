f154303f7606427ef1c9bc24fd0a78171cba76bb
davidodenwald <david.odenwald91@gmail.com>
2023-03-19 18:02:07 +0100

    add test coverage and check html files on watch mode

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
+const { defaults } = require("jest-config");
+
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = () => ({
 	preset: "ts-jest",
+	moduleFileExtensions: [...defaults.moduleFileExtensions, "html"],
+	collectCoverage: true,
 });
