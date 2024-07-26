99e8080f1a5ae2b5426a792878c56ba9b6c849f6
bystray <ibiws0373@yandex.ru>
2024-07-26 09:18:56 +0300

    feat: tests

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -1,44 +1,31 @@
-import { existsSync, readdirSync, readFileSync } from "fs";
+import { readdirSync, readFileSync } from "fs";
 import { join } from "path";
-import { format, Options } from "prettier";
-import * as jinjaPlugin from "../src/index";
+import { format } from "prettier/standalone";
 
-const prettify = (code: string, options: Options) =>
-	format(code, {
-		parser: "jinja-template",
-		plugins: [jinjaPlugin],
-		...options,
-	});
+import { plugin, PLUGIN_KEY } from "../src";
 
-const testFolder = join(__dirname, "cases");
-const tests = readdirSync(testFolder);
+describe("plugins", () => {
+	const testFolder = join(__dirname, "cases");
+	const tests = readdirSync(testFolder).filter((test) => !test.startsWith("_"));
 
-tests.forEach((test) => {
-	if (test.startsWith("_")) {
-		return;
-	}
-	return it(test, async () => {
+	it.each(tests)("should json jinja: %s be valid", async (test) => {
 		const path = join(testFolder, test);
-		const input = readFileSync(join(path, "input.html")).toString();
-		const expected = readFileSync(join(path, "expected.html")).toString();
+		const input = readFileSync(join(path, "input.json")).toString();
+		const expected = readFileSync(join(path, "expected.json")).toString();
 
-		const configPath = join(path, "config.json");
-		const configString =
-			existsSync(configPath) && readFileSync(configPath)?.toString();
-		const configObject = configString ? JSON.parse(configString) : {};
-
-		const format = () => prettify(input, configObject);
+		const formated = () => format(input, {
+			parser: PLUGIN_KEY,
+			plugins: [plugin]
+		});
 
 		const expectedError = expected.match(/Error\(["'`](?<message>.*)["'`]\)/)
 			?.groups?.message;
 
 		if (expectedError) {
-			jest.spyOn(console, "error").mockImplementation(() => {});
-			await expect(format()).rejects.toThrow(expectedError);
-		} else {
-			const result = await format();
-			expect(result).toEqual(expected);
-			expect(await prettify(result, configObject)).toEqual(expected);
+			await expect(formated).rejects.toThrow(expectedError);
+			return;
 		}
+
+		expect(await formated()).toEqual(expected);
 	});
 });
