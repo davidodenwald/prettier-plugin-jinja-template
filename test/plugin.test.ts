cf531cf10b8d44184c891399f051557a2d3175b8
bystray <ibiws0373@yandex.ru>
2024-07-26 10:23:19 +0300

    feat: tests rename

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -6,7 +6,6 @@ import { plugin, PLUGIN_KEY } from "../src";
 
 describe("plugins", () => {
 
-
 	const formated = (input: string) => format(input, {
 		parser: PLUGIN_KEY,
 		plugins: [plugin]
@@ -14,7 +13,7 @@ describe("plugins", () => {
 
 	describe("valid", () => {
 		const validCaseFolder = join(__dirname, "cases");
-		const tests = readdirSync(validCaseFolder).filter((f) => f.endsWith(".json"));
+		const tests = readdirSync(validCaseFolder).filter((f) => f.endsWith(".json.jinja"));
 
 		it.each(tests)("should json jinja: %s be valid", async (test) => {
 			const testPath = join(validCaseFolder, test);
@@ -24,10 +23,10 @@ describe("plugins", () => {
 		});
 	});
 
-	describe("broken", async () => {
+	describe("broken", () => {
 
 		const brokenNotOpenStatementFolder = join(__dirname, "cases", "broken_not_open_statement");
-		const testsBroken = readdirSync(brokenNotOpenStatementFolder).filter((f) => f.endsWith(".json"));
+		const testsBroken = readdirSync(brokenNotOpenStatementFolder).filter((f) => f.endsWith(".json.jinja"));
 
 		it.each(testsBroken)("should json jinja: %s be broken_not_open_statement", async (test) => {
 			const testPath = join(brokenNotOpenStatementFolder, test);
