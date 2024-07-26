70fd4af2dcbe880573c5c4f5592111659a504575
bystray <ibiws0373@yandex.ru>
2024-07-26 10:26:54 +0300

    fix: test

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -13,7 +13,7 @@ describe("plugins", () => {
 
 	describe("valid", () => {
 		const validCaseFolder = join(__dirname, "cases");
-		const tests = readdirSync(validCaseFolder).filter((f) => f.endsWith(".json.jinja"));
+		const tests = readdirSync(validCaseFolder).filter((f) => f.endsWith(".jinja"));
 
 		it.each(tests)("should json jinja: %s be valid", async (test) => {
 			const testPath = join(validCaseFolder, test);
@@ -26,7 +26,7 @@ describe("plugins", () => {
 	describe("broken", () => {
 
 		const brokenNotOpenStatementFolder = join(__dirname, "cases", "broken_not_open_statement");
-		const testsBroken = readdirSync(brokenNotOpenStatementFolder).filter((f) => f.endsWith(".json.jinja"));
+		const testsBroken = readdirSync(brokenNotOpenStatementFolder).filter((f) => f.endsWith(".jinja"));
 
 		it.each(testsBroken)("should json jinja: %s be broken_not_open_statement", async (test) => {
 			const testPath = join(brokenNotOpenStatementFolder, test);
